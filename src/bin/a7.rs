6684c58a1cdec5665ea7f0a9138e1a539993a0da
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-09-26 15:06:02 -0400

    Enum and Struct

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,20 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Yellow,
+    Blue,
+    Green,
+}
+fn print_color(my_color: Color) {
+    match my_color {
+        Color::Red => println!("Red"),
+        Color::Blue => println!("Blue"),
+        Color::Green => println!("Green"),
+        Color::Yellow => println!("Yellow"),
+    }
+}
+fn main() {
+    print_color(Color::Blue);
+}
--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,45 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+// * Use an enum to create different flavors of drinks
+enum Flavor {
+    Sparkling,
+    Sweet,
+    Fruity,
+}
+
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+    // * Use a match expression to print the drink flavor
+    match drink.flavor {
+        Flavor::Sparkling => println!("sparkling"),
+        Flavor::Sweet => println!("sweet"),
+        Flavor::Fruity => println!("fruity"),
+    }
+    println!("oz: {}", drink.fluid_oz);
+}
+fn main() {
+    let sweet = Drink {
+        flavor: Flavor::Sweet,
+        fluid_oz: 6.0,
+    };
+    print_drink(sweet);
+
+    let fruity = Drink {
+        flavor: Flavor::Fruity,
+        fluid_oz: 9.0,
+    };
+    print_drink(fruity);
+
+    let spark = Drink {
+        flavor: Flavor::Sparkling,
+        fluid_oz: 5.60,
+    };
+    print_drink(spark);
+}
