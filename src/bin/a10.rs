550e42817c54249e409a3ff9194edecf8c5273d6
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-10-04 15:50:54 -0400

    intermediate

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -11,5 +11,16 @@
 // * Use a function to print the messages
 // * Use a match expression to determine which message
 //   to print
+fn print_message(gt_100: bool) {
+    match gt_100 {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
 
-fn main() {}
+fn main() {
+    let value = 110;
+    let is_gt_100 = value > 100;
+
+    print_message(is_gt_100);
+}
--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -8,5 +8,25 @@
 // * Use two i32 fields for the quantity and id number
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
+struct GroceryItem {
+    quantity: i32,
+    id: i32,
+}
 
-fn main() {}
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity);
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id);
+}
+
+fn main() {
+    let my_item = GroceryItem {
+        quantity: 3,
+        id: 99,
+    };
+
+    display_quantity(&my_item);
+    display_id(&my_item);
+}
