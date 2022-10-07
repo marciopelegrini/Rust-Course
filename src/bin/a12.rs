7aa7c3c6fc4f32b2f642e6863db43a998d41318f
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-10-06 21:16:29 -0400

    Update a12.rs

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,64 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Green,
+    Red,
+    Blue,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("The color is Red"),
+            Color::Blue => println!("The color is Blue"),
+            Color::Green => println!("The color is Green"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("Width: {:?}", self.width);
+        println!("Height: {:?}", self.height);
+        println!("Depth: {:?}", self.depth);
+    }
+}
+
+struct ShippingBox {
+    color: Color,
+    weight: f64,
+    dimensions: Dimensions,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("Weight: {:?}", self.weight);
+    }
+}
+
+fn main() {
+    let small_dimensions = Dimensions {
+        width: 1.0,
+        height: 2.0,
+        depth: 3.0,
+    };
+
+    let small_box = ShippingBox::new(5.0, Color::Red, small_dimensions);
+    small_box.print();
+}
